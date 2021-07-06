d738dfba14b52198d825f46109540a4ca94701b7
isaias0rt0n <isaiasorton@gmail.com>
2021-07-06 02:43:28 -0300

    autenticando usuarios, concertando header, add novo usuário

--- authentication/settings.py
+++ authentication/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_t$um#bidxt!er*t)bx7vcl_toma#k3b9bq$dig-5o(zw3+mpp'
+SECRET_KEY = 'SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
